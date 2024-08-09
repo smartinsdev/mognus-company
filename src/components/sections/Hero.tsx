6844756969d2aaf9c4fc94c6ab29b24b308d43e0
smartinsdev <sinval.msjunior@gmail.com>
2024-08-09 20:06:31 +0200

    style: add animation into content

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,10 +7,10 @@ export function Hero() {
   return (
     <section className="min-h-dvh max-w-[1440px] mx-auto pt-28 relative flex flex-col items-center">
       <div className="flex flex-col space-y-3 relative mb-6 lg:absolute lg:top-1/2 lg:-translate-y-1/2 lg:left-0 px-4 lg:z-10">
-        <span className="uppercase text-center lg:text-start tracking-widest  text-foreground/70 text-xs lg:text-sm font-semibold">
+        <span className="uppercase animate-in fade-in-0 slide-in-from-left-10 duration-1000 text-center lg:text-start tracking-widest  text-foreground/70 text-xs lg:text-sm font-semibold">
           The Luxury of Made to Measure
         </span>
-        <h1 className="text-3xl sm:text-4xl md:max-w-2xl  lg:max-w-[43rem] text-center lg:text-start  lg:text-5xl xl:text-7xl font-poppins leading-snug capitalize font-extrabold">
+        <h1 className="text-3xl animate-in fade-in-0 slide-in-from-left-10 duration-1000 sm:text-4xl md:max-w-2xl  lg:max-w-[43rem] text-center lg:text-start  lg:text-5xl xl:text-7xl font-poppins leading-snug capitalize font-extrabold">
           Turn your ideas into{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-tr from-orange-400 via-primary to-red-300">
             extraordinary furniture
@@ -18,21 +18,25 @@ export function Hero() {
           with Mognu&apos;s
         </h1>
 
-        <p className="break-words text-center lg:text-start lg:mx-0 text-sm lg:text-base max-w-[60ch] mx-auto pt-2 text-foreground/70 leading-relaxed tracking-wider">
+        <p className="break-words animate-in fade-in-0 slide-in-from-left-10 duration-1000 text-center lg:text-start lg:mx-0 text-sm lg:text-base max-w-[60ch] mx-auto pt-2 text-foreground/70 leading-relaxed tracking-wider">
           We are Mognu&apos;s, a carpentry company specializing in creating
           bespoke furniture. We believe that true luxury lies in having unique
           furniture, made to measure for each client.
         </p>
-        <div className="flex items-center justify-center lg:justify-start gap-2 pt-4">
+        <div className="flex items-center justify-center lg:justify-start gap-2 pt-4 animate-in fade-in-0 slide-in-from-left-10 duration-1000">
           <Button asChild size={"lg"}>
-            <Link href={"/#contact"}>Contact Us</Link>
+            <Link className="uppercase text-xs" href={"/#contact"}>
+              Getting a quote
+            </Link>
           </Button>
           <Button variant={"outline"} size={"lg"}>
-            <Link href={"/#project"}>See our Projects</Link>
+            <Link className="uppercase text-xs" href={"/#project"}>
+              See our Projects
+            </Link>
           </Button>
         </div>
       </div>
-      <div className="relative px-4 lg:absolute w-full lg:w-2/3 lg:right-0 lg:top-1/2 lg:-translate-y-1/2  overflow-hidden rounded ">
+      <div className="relative px-4 lg:absolute w-full lg:w-2/3 lg:right-0 lg:top-1/2 lg:-translate-y-1/2  overflow-hidden rounded animate-in fade-in-0 slide-in-from-right-5 duration-1000">
         <div className="relative">
           <div className="absolute top-0 left-0 h-full w-full bg-transparent dark:bg-black/50"></div>
           <Image
